ec67bf58e66059bebfae5323fceadb5c430c6169
Piyush Verma <piyushv080@gmail.com>
2022-02-13 16:35:11 +0530

    save to remote server

---
+++ module.py
@@ -0,0 +1,20 @@
+#built in module
+
+import os
+# #print(dir(os))
+# print(os.getcwd())
+
+# # changing current working directory
+# # os.chdir("C://")
+# print(os.getcwd())
+
+# print(os.listdir("e://"))
+
+# os.mkdir("This")
+# os.makedirs("This/that")
+
+# os.rename("1.hello.py" , "1.Helloworld.py")
+
+# print(os.environ.get('HOME'))
+# print(os.environ.get('Path'))
+print(os.path.join("C://","/piyush.txt")) 
---
+++ user_input.py
@@ -0,0 +1,4 @@
+
+user_input = input("Hi User, Enter a Number!\n")
+print(user_input)
+  
